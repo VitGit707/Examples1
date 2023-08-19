4f8337c87bdea4a907f33360850b29ffa739d552
Vitaly Zimin <infovitz7@gmail.com>
2023-08-19 16:15:20 +0300

    Example002_Hello User

--- Example002_Hello User/Program.cs
+++ Example002_Hello User/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Example002_Hello User
+
+Console.WriteLine("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+//есть еще команда, называемая: Console.Write
+Console.Write("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
