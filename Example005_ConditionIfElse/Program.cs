2a2b7a203fa13485fb016a88bb716db0d07a18e6
Vitaly Zimin <infovitz7@gmail.com>
2023-08-19 16:47:48 +0300

    Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.WriteLine("Example005_ConditionIfElse!");
+
+Console.WriteLine("Введите Имя Пользователя: "); // Делаем приглашение ко вводу (просим ввести имя пользователя) 
+string username = Console.ReadLine(); // Считываем данныем мы при помощи Console.ReadLine()
+
+if (username.ToLower() == "маша")  // Мы хотим сделать проверку имени ползователя
+{
+    Console.WriteLine("Ура, это же Маша!!!!!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
