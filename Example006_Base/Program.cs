1329fd37b19bbb90609261454ddcf31c27b4c83b
Vitaly Zimin <infovitz7@gmail.com>
2023-08-19 23:04:43 +0300

    Example006_Base/

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,21 @@
+﻿// Console.WriteLine("EXAMPLE006_BASE !!!");
+
+int a = 3;
+int b = 5;
+int c = 16;
+int d = 8;
+int e = 14;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+
